997db713a17cd2f57cd78b835d2ad203bef13008
Karthikeyan <karthikeyanagaram2004@gmail.com>
2026-05-25 16:46:15 +0530

    fixed render backend

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,15 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root%40123@localhost:5432/vizhuthugaldb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
