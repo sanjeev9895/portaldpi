49df128de8067a7b40baefd12719a6bd31c09636
Karthikeyan <karthikeyanagaram2004@gmail.com>
2026-05-26 13:37:36 +0530

    updated ambassadorlist

--- src/pages/communityteam/ambassadorpage/ambassadorlist.tsx
+++ src/pages/communityteam/ambassadorpage/ambassadorlist.tsx
@@ -15,10 +15,6 @@ import {
     useState,
 } from 'react'
 
-import {
-    useNavigate,
-} from 'react-router-dom'
-
 import api from '../../../services/api'
 
 import BackButton from '../../../components/BackButton'
